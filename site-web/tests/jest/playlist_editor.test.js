c70f533dd72012a718b67c43be19576f55240e4a
Simon Denis <simon.denis@polymtl.ca>
2022-11-06 17:42:04 -0500

    reste le dernier qui marche pas jfais sa soon

--- site-web/tests/jest/playlist_editor.test.js
+++ site-web/tests/jest/playlist_editor.test.js
@@ -166,11 +166,44 @@ describe("Playlist Editor tests", () => {
   });
 
   it("load should correctly add addItemSelect as eventListener on click event to add song button", () => {
-    // TODO
+    // TODO DONE
+    const addSongButton = document.getElementById("add-song-btn");
+    const addSongButtonAddEventListener = jest.spyOn(addSongButton, "addEventListener").mockImplementation(() => {});
+    jest.spyOn(playListEditor, "buildDataList").mockImplementation(() => {});
+    jest.spyOn(playListEditor.storageManager,"loadAllData").mockImplementation(() => {});
+    jest.spyOn(playListEditor.storageManager, "getData").mockImplementation(() => {});
+
+    playListEditor.load();
+
+    expect(addSongButtonAddEventListener).toBeCalled();
+    expect(addSongButtonAddEventListener).toBeCalledWith("click", playListEditor.addItemSelect);
+
+
   });
 
   it("load should correctly call createPlaylist and preventDefault on submit event to the form", () => {
     // TODO
+    const form = document.getElementById("playlist-form");
+    const formAddEventListener = jest.spyOn(form, "addEventListener").mockImplementation(() => {});
+
+    jest.spyOn(playListEditor, "buildDataList").mockImplementation(() => {});
+    jest.spyOn(playListEditor.storageManager,"loadAllData").mockImplementation(() => {});
+    jest.spyOn(playListEditor.storageManager, "getData").mockImplementation(() => {});
+    const createPlaylistSpy = jest.spyOn(playListEditor, "createPlaylist").mockImplementation(() => {});
+
+    const submitEvent = new Event("submit");
+    const submitEventPreventDefaultSpy = jest.spyOn(submitEvent, "preventDefault");
+
+    playListEditor.load();
+
+    expect(formAddEventListener).toBeCalled();
+
+    form.submit();
+
+    // expect(submitEventPreventDefaultSpy).toBeCalled();
+    expect(createPlaylistSpy).toBeCalled();
+
+    
   });
 
   it("createPlaylist should correctly call getImageInput, StorageManager.getIdFromName & StorageManager.addItem", async () => {
